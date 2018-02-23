b4700965cc584f09661b6d5edd2d7d163becd27a
jagerb0mb <maxime.callico@gmail.com>
2018-02-23 14:45:36 +0100

    all getdocs from lucenebuilder added

--- src/es/uam/eps/bmi/search/index/lucene/LuceneBuilder.java
+++ src/es/uam/eps/bmi/search/index/lucene/LuceneBuilder.java
@@ -1,34 +1,116 @@
 package es.uam.eps.bmi.search.index.lucene;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.TextField;
 
 import es.uam.eps.bmi.search.index.IndexBuilder;
 
 public class LuceneBuilder implements IndexBuilder {
-
-	private Document getDocument (File file) throws IOException {
-		Document doc = new Document ();
-		FieldType base = new FieldType ();
-		base.setStored(true);
-		base.setTokenized(true);
-		Field contenido = new Field ("contenido", file.toString(), base);
-		Field nombre = new Field ("nombre", file.getName(), base);
-		doc.add(contenido);
-		doc.add(nombre);
-		return doc;
+	
+	// la méthode qui renvoie les fields d'une file
+	public ArrayList<TextField> getFieldsFromFile(File file) throws IOException {
+		
+		ArrayList<TextField> fields = new ArrayList<TextField>();
+		FileReader fileReader = new FileReader(file);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		StringBuffer stringBuffer = new StringBuffer();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			stringBuffer.append(line);
+			fields.add(new TextField("content", line, Field.Store.YES));
+		}
+		fileReader.close();
+		
+		return fields;
 	}
 	
+	// celle ci renvoie le document créé a partir d'un zip
+	public Document getDocument (ZipFile zip) throws IOException {
+		
+		Document doc = new Document ();
+		TextField path = new TextField("path", zip.getName(), Field.Store.YES);
+		doc.add(path);
+		
+		Enumeration<? extends ZipEntry> entries = zip.entries();
+
+	    while(entries.hasMoreElements()){
+	        ZipEntry entry = entries.nextElement();
+	        InputStream stream = zip.getInputStream(entry);
+	        
+	        StringBuilder out = new StringBuilder();
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+	        String line;
+	        try {
+	            while ((line = reader.readLine()) != null) {
+	                out.append(line);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        File file = new File("");
+	        Writer writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+	        writer.write(out.toString());
+	        writer.close();
+
+	        for(TextField field : getFieldsFromFile(file)) {
+				doc.add(field);
+			}
+			stream.close();
+	    }
+	    
+	    return doc;
+	    
+	}
 	
-	
-	
-	
-	
+	// celle ci renvoie le document à partir d'une file, que ce soit un fichier ou une file
+	public Document getDocument (File file) throws IOException{
+		
+		Document doc = new Document ();
+		TextField path = new TextField("path", file.getCanonicalPath(), Field.Store.YES);
+		doc.add(path);
+		
+		if (file.isDirectory()) {
+			File dir = new File(file.getCanonicalPath());
+			File[] filesInDir = dir.listFiles();
+			if (filesInDir != null) {
+				for (File child : filesInDir) {
+					if (!(child.isDirectory())) {
+						for(TextField field : getFieldsFromFile(child)) {
+							doc.add(field);
+						}
+					}
+				}
+			}
+		}
+		
+		else {
+			for(TextField field : getFieldsFromFile(file)) {
+				doc.add(field);
+			}
+		}
+		
+		return doc;
+	}
 	
 	@Override
 	public void build(String collectionPath, String indexPath) throws IOException {
