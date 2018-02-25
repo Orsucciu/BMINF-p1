36be9c8d47f4358f56b5c0b28b799addaf04d41c
jagerb0mb <maxime.callico@gmail.com>
2018-02-25 19:49:06 +0100

    luceneengine done
    (check if too much)

--- src/es/uam/eps/bmi/search/lucene/LuceneEngine.java
+++ src/es/uam/eps/bmi/search/lucene/LuceneEngine.java
@@ -1,5 +1,35 @@
 package es.uam.eps.bmi.search.lucene;
 
-public class LuceneEngine {
+public class LuceneEngine extends AbstractEngine {
 
+	private LuceneIndex index;
+	private IndexSearcher searcher;
+	
+	public LuceneEngine (String path) throws IOException {
+		super(path)
+	}
+	
+	@Override
+	public SearchRanking search (String petition, int quantity) throws IOException {
+		
+		// we create the query
+		
+		BooleanQuery.Builder builder = new BooleanQuery.Builder();
+		String[] terms = petition.split(" ");
+		for (int i = 0; i < terms.length; i++) {
+			TermQuery termpetition = new TermQuery (new Term ("content", i));
+			builder.add (termpetition);
+		}
+		
+		BooleanQuery query = builder.build();
+		TopDocs results = searcher.search (query, quantity);
+		return new LuceneRanking (index, results.scoreDocs)
+	}
+	
+	@Override
+	public void loadIndex (String path) throws IOException {
+		index = new LuceneIndex (path);
+		searcher = new IndexSearcher (index.getIndexReader());
+	}
+	
 }
