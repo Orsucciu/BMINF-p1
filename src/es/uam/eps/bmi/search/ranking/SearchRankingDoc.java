e7a7ff9ee6a8cd2214b94a8debf5f8ce71585b7e
jagerb0mb <maxime.callico@gmail.com>
2018-02-25 20:14:03 +0100

    luceneranking, searchrankingdoc

--- src/es/uam/eps/bmi/search/ranking/SearchRankingDoc.java
+++ src/es/uam/eps/bmi/search/ranking/SearchRankingDoc.java
@@ -1,5 +1,20 @@
 package es.uam.eps.bmi.search.ranking;
 
 public class SearchRankingDoc implements Comparable<T> {
+	
+	private ScoreDoc scoredoc;
+	private String path;
+	
+	public SearchRankingDoc (ScoreDoc scoredoc, String path) {
+		this.scoredoc = scoredoc;
+		this.path = path;
+	}
+	
+	@Override
+	public int compareTo (SearchRankingDoc o) {
+		if (scoredoc.rankedDoc.doc > o.getScoreDoc().doc) return -1;
+		else if (scoredoc.rankedDoc.doc < o.getScoreDoc().doc) return 1;
+		return 0;
+	}
 
 }
--- src/es/uam/eps/bmi/search/ranking/lucene/LuceneRanking.java
+++ src/es/uam/eps/bmi/search/ranking/lucene/LuceneRanking.java
@@ -1,5 +1,16 @@
 package es.uam.eps.bmi.search.ranking.lucene;
 
-public class LuceneRanking {
+public class LuceneRanking implements SearchRanking {
+	
+	private LuceneRankingIterator iterator;
+	
+	public LuceneRanking (LuceneIndex index, ScoreDoc[] scores) {
+		iterator = new LuceneRankingIterator (index, scores)
+	}
+	
+	@Override
+	public int size () {
+		return iterator.results.length();
+	}
 
 }
